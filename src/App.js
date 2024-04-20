229f63cdac6abd581e15dc9317cc8221ae6a5402
jjini-SUN <shineeseo@naver.com>
2024-04-20 12:57:42 +0900

    2024.04.20 shop

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Container, Nav, Navbar, Button} from 'react-bootstrap'
+import {Container, Col, Row, Nav, Navbar, Button} from 'react-bootstrap'
 import './App.css';
+import { useState } from 'react';
+
 // css를 제공해주는 사이트 : bootstrap
 // npm install react-bootstrap bootstrap
 
@@ -10,25 +12,81 @@ import './App.css';
 import data from './data'
 import {num1, num2, num3} from './data.js'
 
+//이미지를 사용하려면  import
+import mainBG from './배너.jpg';  //이미지 import
+
+
 function App() {
+
+  let [items, setItems] = useState(data);  //data는 변수니깐 state로
+  let [photo, setphoto] = useState(['/아이쉐도우.jpg', '/립스틱.jpg', '/브러쉬.jpg']);  //useState import
+
 
   return (
     <div className="App">
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">{data}</Navbar.Brand>
+          <Navbar.Brand href="#home">
+            <img src = '/horse.png' width='120px' height = '50px' />
+          </Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">{num1}</Nav.Link>
-            <Nav.Link href="#features">{num2}</Nav.Link>
-            <Nav.Link href="#pricing">{num3}</Nav.Link>
+            <Nav.Link href="#home">홈</Nav.Link>
+            <Nav.Link href="#features">상세페이지</Nav.Link>
+            <Nav.Link href="#pricing">About</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
 
+      <div className = {'main-bg'}>
+
+      </div>
+
+      <Container>
+        <Row>
+          {/* data개수와 image가 바뀔 수 있으니까 useState 처리
+          data : 변수 (사용자 입력 또는 서버로부터 데이터를 받았을 때 변경이 되어도 화면은 안바귐 ==> useState로 변경)
+            map을 통해서 useState(data)만큼 반복 */}
+          {items.map((item, idx) => (
+            <ItemCol key={idx} data={item} img={photo[idx]} />
+          ))}
+          {/* <ItemCol data = {items[0]} img = {photo[0]}/>
+          <ItemCol data = {items[1]} img = {photo[1]}/>
+          <ItemCol data = {items[2]} img = {photo[2]}/> */}
+s
+        </Row>
+        <Row>
+          <Col>1</Col>
+          <Col>2</Col>
+          <Col>3</Col>
+        </Row>
+      </Container>
+
+      {/* <div style = {{backgroundImage:`url(${mainBG})`, height:'350px', width:'100%', backgroundSize:'cover', backgroundPosition:'center'}}>
+      </div>
+      아니면  `url(/logo192.png)`*/}
+
       <br/>
-      <Button variant="primary">Primary</Button>{' '}
+      <Button variant="primary">Buy Now</Button>{' '}
     </div>
   );
 }
 
 export default App;
+
+
+//컴포넌트는 맨 앞글자는 대문자
+//return에 html(jsx)코드
+//컴포넌트로 분리를 했으면 props로 받아와야 하는 부분을 변경
+function ItemCol(Props)
+{
+  //사용하는 곳에서 결정해줘야 하는 부분(그때그때 바뀌어야 하는 부분)만 props 처리
+  return(
+    <>
+      <Col>
+            <img src = {Props.img} width={'300px'} height={'200px'} />
+            <h4>{Props.data.title}</h4>
+            <p>{Props.data.price} 원</p>
+          </Col>
+    </>
+  )
+}
--- src/data.js
+++ src/data.js
@@ -1,4 +1,23 @@
-let data = 10;
+let data = [
+  {
+    id:0,
+    title:'아이쉐도우',
+    content:'UV 차단 30',
+    price:8000
+  },
+  {
+    id:1,
+    title:'립스틱',
+    content:'뛰어난 발색',
+    price:15000
+  },
+  {
+    id:2,
+    title:'브러쉬',
+    content:'피부 자극 전혀 없음',
+    price:6000
+  },
+];
 let num1 = 1;
 let num2 = 2;
 let num3 = 3;
